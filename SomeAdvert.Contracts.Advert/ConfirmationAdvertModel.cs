7bf2e68f9fa49f3711f21d36db2b4f1eafd53fc0
alex-rsppv <rsppv.av@gmail.com>
2023-01-03 01:45:29 +0400

    formatting

---
+++ SomeAdvert.Contracts.Advert/ConfirmationAdvertModel.cs
@@ -0,0 +1,14 @@
+﻿namespace SomeAdvert.Contracts.Advert
+{
+    public enum AdvertStatus
+    {
+        Pending = 1,
+        Active
+    }
+
+    public class ConfirmationAdvertModel
+    {
+        public string AdvertId { get; set; } = string.Empty;
+        public AdvertStatus Status { get; set; }
+    }
+}
