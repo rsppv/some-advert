a86e2f34280b90ba0107da5e03afd8a017dbc390
alex-rsppv <rsppv.av@gmail.com>
2023-01-02 21:48:14 +0400

    add dummy AdvertStorage class with Amazon DynamoDB SDK

---
+++ SomeAdvert.Api.Advert/Services/AdvertStorage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2.DataModel;
+using SomeAdvert.Contracts.Advert;
+
+namespace SomeAdvert.Api.Advert.Services
+{
+    [DynamoDBTable("Adverts")]
+    public class AdvertDbModel
+    {
+        [DynamoDBHashKey]
+        public string Id { get; set; }
+        
+        [DynamoDBProperty]
+        public string Title { get; set; }
+        
+        [DynamoDBProperty]
+        public string Description { get; set; }
+        
+        [DynamoDBProperty]
+        public decimal Price { get; set; }
+        
+        [DynamoDBProperty]
+        public DateTime CreatedAt { get; set; }
+        
+        [DynamoDBProperty]
+        public AdvertStatus Status { get; set; }
+    }
+    
+    public interface IAdvertStorage
+    {
+        Task Add(AdvertModel advert);
+        Task Confirm(ConfirmationAdvertModel confirmation);
+    }
+
+    public class DynamoDbAdvertStorage : IAdvertStorage
+    {
+        public Task Add(AdvertModel advert)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Confirm(ConfirmationAdvertModel confirmation)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
