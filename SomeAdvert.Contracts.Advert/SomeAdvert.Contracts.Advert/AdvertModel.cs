aac80b41efd15b232b9b294526cc888b59e3aa66
alex-rsppv <rsppv.av@gmail.com>
2023-01-02 18:15:08 +0400

    add contracts project with models

---
+++ SomeAdvert.Contracts.Advert/SomeAdvert.Contracts.Advert/AdvertModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SomeAdvert.Contracts.Advert
+{
+    public class AdvertModel
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
